dd73190142a49a522c29b6235cb704c759aab789
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-06-02 22:20:25 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -9,45 +9,37 @@ public class PointTest {
 
     @Test
     public void when00To20Then2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
         double expected = 2;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when01To10Then1dot41() {
-        int x1 = 0;
-        int y1 = 1;
-        int x2 = 1;
-        int y2 = 0;
+        Point a = new Point(0, 1);
+        Point b = new Point(1, 0);
         double expected = 1.41;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when0minus1ToMinus10Then1dot41() {
-        int x1 = 0;
-        int y1 = -1;
-        int x2 = -1;
-        int y2 = 0;
+        Point a = new Point(0, -1);
+        Point b = new Point(-1, 0);
         double expected = 1.41;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when23To32Then2dot23() {
-        int x1 = 2;
-        int y1 = 3;
-        int x2 = 0;
-        int y2 = 2;
+        Point a = new Point(2, 3);
+        Point b = new Point(0, 2);
         double expected = 2.23;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
